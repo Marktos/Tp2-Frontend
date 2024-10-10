e5f9549012ae507246a94062991637c6a9559cb9
Marcos Neculman <marcosneculman040@gmail.com>
2024-10-10 10:23:36 -0300

    Form login habiendo corrigido los errores de la tarea pasada

--- src/models/UserModel.ts
+++ src/models/UserModel.ts
@@ -1,11 +1,5 @@
 export interface User {
-    id: number,
-    firstName: string,
-    lastName: string,
     username: string,
     password: string,
-    rememberMe?: boolean,
-    isAdmin: boolean,
-    jwtToken?: string,
-    refreshTokens: string[]
+    rememberMe: boolean,
 }
